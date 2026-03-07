a518a2fea2154eb28c57442c6f961a164a1f4540
Ber3erg <Petruha1029@gmail.com>
2026-03-07 17:46:18 +0300

    Добавлены остальные файлы в проект в том числе парсер

---
+++ IcalParser.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IcalDemo
+{
+    public class IcalParser
+    {
+        public List<Event> Parse(string filePath)
+        {
+            var events = new List<Event>();
+            Event current = null;
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                if (line.StartsWith("BEGIN:VEVENT"))
+                    current = new Event();
+
+                else if (line.StartsWith("SUMMARY:"))
+                    current.Summary = line.Replace("SUMMARY:", "");
+
+                else if (line.StartsWith("DTSTART:"))
+                    current.Start = ParseDate(line.Replace("DTSTART:", ""));
+
+                else if (line.StartsWith("DTEND:"))
+                    current.End = ParseDate(line.Replace("DTEND:", ""));
+
+                else if (line.StartsWith("END:VEVENT"))
+                    events.Add(current);
+            }
+
+            return events;
+        }
+
+        private DateTime ParseDate(string value)
+        {
+            return DateTime.ParseExact(value, "yyyyMMdd'T'HHmmss", null);
+        }
+    }
+}
