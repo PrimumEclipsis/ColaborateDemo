b806e631a760f93f6d5fc868fc79932be12e6294
Ber3erg <Petruha1029@gmail.com>
2026-03-07 17:44:30 +0300

    Добавлен файл gitignore program файл и event модель

---
+++ Event.cs
@@ -0,0 +1,9 @@
+namespace IcalDemo
+{
+    public class Event
+    {
+        public string Summary { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End {get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using IcalDemo;
+var parser = new IcalParser();
+var events = parser.Parse("sample.ics");
+
+foreach (var e in events)
+{
+    Console.WriteLine($"Event: {e.Summary}");
+    Console.WriteLine($"Start: {e.Start}");
+    Console.WriteLine($"End: {e.End}");
+    Console.WriteLine();
+}
